b0884aa55a8afe6f10fe8459ac437bb55dc457ac
Martin Barker <martin-barker@hotmail.co.uk>
2016-04-26 03:15:53 +0100

    code has comments added
    Code is commented and fixed a bug in missing $this

--- pgp.php
+++ pgp.php
@@ -1,22 +1,32 @@
 <?php
 class CodeGolf{
+	// constuct get access to $argv from global scope
 	public function __construct(&$argv){
-		$filePath = $argv[1];
-		unset($argv[1]);
+		// read the input
+		$inp = $argv[1];
+		// get rid of pgp.pgp and shift the array keys to be in the right place
+		$argv[0] = $inp;
+		unset($argv[0]);
 		$argv = array_values($argv);
-		if(file_exists($filepath)){
-			$this->loadFile($filePath);
+		// check if it's a file
+		if(file_exists($inp)){
+			// yes load the file
+			$this->loadFile($inp);
 		}else{
-			run($filePath);
+			// no then it must be raw pgp try and run it
+			$this->run($inp);
 		}
 	}
 
 	private function loadFile($filepath){
+		// call run with the file contents
 		$this->run(file_get_contents($filepath));
 	}
 
 	private function run($inp){
+		// enable access to $argv
 		global $argv;
+		// replace short hand pgp with php code
 		$replace = array(
 				"func(" => "function(",
 				"f(" => "for(",
@@ -28,9 +38,13 @@ class CodeGolf{
 				"p(" => "print(",
 		);
 		$inp = str_replace(array_keys($replace), array_values($replace), $inp);
+		// run the code if it returns caputre it
 		$ret = eval($inp);
+		// echo out the return
 		echo $ret;
 	}
 }
+// start the CodeGolf
 new CodeGolf($argv);
+// make sure we keep newline at end of output for console
 echo "\r\n";
